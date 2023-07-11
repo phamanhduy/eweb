d13ea979ba34c564865c0fbdd8df21ad249f60ce
phamanhduy <duypham965@gmail.com>
2023-07-11 23:50:26 +0700

    first cm

---
+++ public/js/lesson.js
@@ -0,0 +1,93 @@
+var lessons = [{
+  id: '1',
+  title: 'Đăng ký tên miền',
+  video: 'https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4',
+  description: `<video width='100%' src="https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4" controls></video>
+  <h1 id="ea3e002196e8f6f725c5d70c523fa9da6">Discover the riches of our editor ✨</h1>
+  Read on to better understand the functionalities you can test with this demo.
+  <h2 id="e64358fd18bbc9632ae1f4934d8fd95ea">💡 Did you know that…</h2>
+  <ul>
+     <li>CKEditor is <strong>customizable</strong>. You can fine-tune the set of enabled plugins, available toolbar buttons, and the behavior of features.</li>
+     <li>The editor supports <strong>@mentions</strong>. Start typing <code>@An</code> to see available suggestions.</li>
+     <li>You can <strong>paste content</strong> from Word or Google Docs, retaining the original document structure and formatting.</li>
+     <li>Thanks to Import from Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png 2x" alt="Import from Word" width="20" height="20" />, you can <strong>convert</strong> a DOCX document into HTML and edit it in CKEditor 5.</li>
+     <li>You can <strong>export your document</strong> to PDF <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png 2x" alt="Export to PDF" width="20" height="20" /> or Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png 2x" alt="Export to Word" width="20" height="20" /> with a single click.</li>
+     <li>This demo showcases <a href="https://ckeditor.com/ckbox/" target="_blank" rel="noopener noreferrer">CKBox</a> <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png 2x" alt="Browse files" width="20" height="20" /> to <strong>manage images and other files</strong>. You can enable your own upload adapter instead.</li>
+  </ul>
+  <h2 id="e72c088e78c74254b52e0bc85310e2ed6">🚀 Autoformatting in CKEditor 5</h2>
+  Some features of CKEditor 5 might be hard to spot at first glance. Thanks to <strong>autoformatting</strong>, you can use handy shortcuts in the editor to format the text on the fly:
+  <figure class="table">
+  <table class="ck-table-resized"><colgroup> <col /> <col /> <col /></colgroup>
+  <tbody>
+  <tr>
+  <td rowspan="7"><strong>Block formatting</strong></td>
+  <td>Bulleted list</td>
+  <td>Start a line with <code>*</code> or <code>-</code> followed by a space.</td>
+  </tr>
+  <tr>
+  <td>Numbered list</td>
+  <td>Start a line with <code>1.</code> or <code>1)</code> followed by a space.</td>
+  </tr>
+  <tr>
+  <td>To-do list</td>
+  <td>Start a line with <code>[ ]</code> or <code>[x]</code> followed by a space to insert an unchecked or checked list item.</td>
+  </tr>
+  <tr>
+  <td>Headings</td>
+  <td>Start a line with <code>#</code>, <code>##</code>, or <code>###</code> followed by a space to create a heading 1, heading 2, or heading 3.</td>
+  </tr>
+  <tr>
+  <td>Block quote</td>
+  <td>Start a line with <code>&gt;</code> followed by a space.</td>
+  </tr>
+  <tr>
+  <td>Code block</td>
+  <td>Start a line with <code></code>.</td>
+  </tr>
+  <tr>
+  <td>Horizontal line</td>
+  <td>Start a line with <code>---</code>.</td>
+  </tr>
+  <tr>
+  <td rowspan="4"><strong>Inline formatting</strong></td>
+  <td><strong>Bold</strong></td>
+  <td>Type <code>**</code> or <code>__</code> around your text.</td>
+  </tr>
+  <tr>
+  <td><i>Italic</i></td>
+  <td>Type <code>*</code> or <code>_</code> around your text.</td>
+  </tr>
+  <tr>
+  <td><code>Code</code></td>
+  <td>Type <code>
+  </code> around your text.</td>
+  </tr>
+  <tr>
+  <td><s>Strikethrough</s></td>
+  <td>Type <code>~~</code> around your text.</td>
+  </tr>
+  </tbody>
+  </table>
+  </figure>
+  &nbsp;`
+},
+{
+  id: '2',
+  title: 'Đăng ký hosting',
+  video: 'https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4',
+  description: `<video width='100%' src="https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4" controls></video>
+  <h1 id="ea3e002196e8f6f725c5d70c523fa9da6">Discover the riches of our editor ✨</h1>
+  Read on to better understand the functionalities you can test with this demo.
+  <h2 id="e64358fd18bbc9632ae1f4934d8fd95ea">💡 Did you know that…</h2>
+  <ul>
+     <li>CKEditor is <strong>customizable</strong>. You can fine-tune the set of enabled plugins, available toolbar buttons, and the behavior of features.</li>
+     <li>The editor supports <strong>@mentions</strong>. Start typing <code>@An</code> to see available suggestions.</li>
+     <li>You can <strong>paste content</strong> from Word or Google Docs, retaining the original document structure and formatting.</li>
+     <li>Thanks to Import from Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png 2x" alt="Import from Word" width="20" height="20" />, you can <strong>convert</strong> a DOCX document into HTML and edit it in CKEditor 5.</li>
+     <li>You can <strong>export your document</strong> to PDF <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png 2x" alt="Export to PDF" width="20" height="20" /> or Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png 2x" alt="Export to Word" width="20" height="20" /> with a single click.</li>
+     <li>This demo showcases <a href="https://ckeditor.com/ckbox/" target="_blank" rel="noopener noreferrer">CKBox</a> <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png 2x" alt="Browse files" width="20" height="20" /> to <strong>manage images and other files</strong>. You can enable your own upload adapter instead.</li>
+  </ul>
+  &nbsp;`
+}
+]
+
