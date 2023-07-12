592b11c4c0d616e181593cdc7836cd3848941b5c
phamanhduy <duypham965@gmail.com>
2023-07-13 00:00:11 +0700

    videojs

--- public/js/lesson.js
+++ public/js/lesson.js
@@ -6,21 +6,72 @@ var lessons = [{
   <source src="https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4" type="video/mp4">
   </video>
   <br>
-  <br>
-  <strong>1. TÊN MIỀN LÀ GÌ?</strong>  <br>
-Tên miền là một địa chỉ trên internet giúp chúng ta truy cập vào các trang web khác nhau. Nó giống như địa chỉ nhà của bạn trên mạng. Khi bạn muốn truy cập vào một trang web cụ thể, bạn chỉ cần gõ tên miền vào thanh địa chỉ của trình duyệt và trang web sẽ hiển thị trước mắt bạn.
-
-Ví dụ, hãy nghĩ về tên miền như là một cái tên độc đáo cho trang web của bạn. Nó có thể là cái tên của doanh nghiệp, cá nhân hoặc tổ chức của bạn. Bạn có thể chọn tên miền theo ý muốn của mình, nhưng cũng cần lưu ý một số quy tắc.
-
-2. CÁCH CHỌN TÊN MIỀN
-<ol>
- 	<li>Đơn giản và dễ nhớ: Chọn một tên miền ngắn, dễ nhớ và dễ đánh vần. Điều này giúp người dùng dễ dàng nhớ và gõ đúng tên miền của bạn vào trình duyệt.</li>
- 	<li>Phù hợp với nội dung trang web: Tên miền nên phản ánh một cách rõ ràng về nội dung hoặc hoạt động của trang web của bạn. Ví dụ, nếu bạn kinh doanh sách, tên miền có thể là "<a href="http://www.banchinhem.vn/" target="_new">www.banchinhem.vn</a>" để thể hiện rõ rằng bạn bán sách.</li>
- 	<li>Chọn phần đuôi phù hợp: Phần đuôi (TLD) là phần sau dấu chấm trong tên miền, ví dụ như .com, .net, .org. Hãy chọn một phần đuôi phù hợp với mục đích sử dụng của bạn. Phần đuôi .com thường được sử dụng rộng rãi và phổ biến nhất.</li>
-</ol>
-Để chọn một tên miền hay, bạn có thể nghĩ về lĩnh vực hoạt động của bạn, những từ khóa liên quan và cố gắng kết hợp chúng vào tên miền. Đừng quên kiểm tra tính khả dụng của tên miền trên các trang web đăng ký tên miền để đảm bảo rằng nó chưa được sử dụng bởi ai khác.
-
-<strong>VIDEO BÊN TRÊN SẼ HƯỚNG DẪN BẠN ĐĂNG KÝ 1 TÊN MIỀN NHANH CHÓNG TẠI TENTEN</strong>`
+  <h1 id="e2eeea5c31e438d10f52993297f5edc06">Discover the riches of our editor ✨</h1>
+  Read on to better understand the functionalities you can test with this demo.
+  <h2 id="e9ae557e2bd3e09bad12bcdb00cd6ee0c">💡 Did you know that…</h2>
+  <ul>
+     <li>CKEditor is <strong>customizable</strong>. You can fine-tune the set of enabled plugins, available toolbar buttons, and the behavior of features.</li>
+     <li>The editor supports <strong>@mentions</strong>. Start typing <code>@An</code> to see available suggestions.</li>
+     <li>You can <strong>paste content</strong> from Word or Google Docs, retaining the original document structure and formatting.</li>
+     <li>Thanks to Import from Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png 2x" alt="Import from Word" width="20" height="20" />, you can <strong>convert</strong> a DOCX document into HTML and edit it in CKEditor 5.</li>
+     <li>You can <strong>export your document</strong> to PDF <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png 2x" alt="Export to PDF" width="20" height="20" /> or Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png 2x" alt="Export to Word" width="20" height="20" /> with a single click.</li>
+     <li>This demo showcases <a href="https://ckeditor.com/ckbox/" target="_blank" rel="noopener noreferrer">CKBox</a> <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png 2x" alt="Browse files" width="20" height="20" /> to <strong>manage images and other files</strong>. You can enable your own upload adapter instead.</li>
+  </ul>
+  <h2 id="e3110f5a48c832f6e1c5696c439440fb6">🚀 Autoformatting in CKEditor 5</h2>
+  Some features of CKEditor 5 might be hard to spot at first glance. Thanks to <strong>autoformatting</strong>, you can use handy shortcuts in the editor to format the text on the fly:
+  <figure class="table">
+  <table class="ck-table-resized"><colgroup><col /><col /><col /></colgroup>
+  <tbody>
+  <tr>
+  <td rowspan="7"><strong>Block formatting</strong></td>
+  <td>Bulleted list</td>
+  <td>Start a line with <code>*</code> or <code>-</code> followed by a space.</td>
+  </tr>
+  <tr>
+  <td>Numbered list</td>
+  <td>Start a line with <code>1.</code> or <code>1)</code> followed by a space.</td>
+  </tr>
+  <tr>
+  <td>To-do list</td>
+  <td>Start a line with <code>[ ]</code> or <code>[x]</code> followed by a space to insert an unchecked or checked list item.</td>
+  </tr>
+  <tr>
+  <td>Headings</td>
+  <td>Start a line with <code>#</code>, <code>##</code>, or <code>###</code> followed by a space to create a heading 1, heading 2, or heading 3.</td>
+  </tr>
+  <tr>
+  <td>Block quote</td>
+  <td>Start a line with <code>&gt;</code> followed by a space.</td>
+  </tr>
+  <tr>
+  <td>Code block</td>
+  <td>Start a line with <code></code>.</td>
+  </tr>
+  <tr>
+  <td>Horizontal line</td>
+  <td>Start a line with <code>---</code>.</td>
+  </tr>
+  <tr>
+  <td rowspan="4"><strong>Inline formatting</strong></td>
+  <td><strong>Bold</strong></td>
+  <td>Type <code>**</code> or <code>__</code> around your text.</td>
+  </tr>
+  <tr>
+  <td><i>Italic</i></td>
+  <td>Type <code>*</code> or <code>_</code> around your text.</td>
+  </tr>
+  <tr>
+  <td><code>Code</code></td>
+  <td>Type <code></code> around your text.</td>
+  </tr>
+  <tr>
+  <td><s>Strikethrough</s></td>
+  <td>Type <code>~~</code> around your text.</td>
+  </tr>
+  </tbody>
+  </table>
+  </figure>
+  &nbsp;`
 },
 {
   id: '2',
