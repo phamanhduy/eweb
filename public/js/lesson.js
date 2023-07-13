2ddb59bf6ccf1580eaf9bf2e66fab9151eb37ce8
duypham1-ntq <duy.pham1@ntq-solution.com.vn>
2023-07-13 17:18:45 +0700

    co

--- public/js/lesson.js
+++ public/js/lesson.js
@@ -1,84 +1,46 @@
-var lessons = [{
-  id: '1',
-  title: 'Đăng ký tên miền',
+var lessons = [
+  {
+    id: '1',
+    sidebar: 'Tại sao nên tự tạo wesite?',
+    title: 'Tại sao nên tự tạo wesite?',
+    video: 'https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4',
+    description: `<video id='video-js1' key='video-js1' class="video-js" data-setup='{"fluid": true}' controls>
+    <source src="https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4" type="video/mp4">
+    </video>
+    <hr>
+    <h2>💡 Tại sao nên tự làm website</h2><ol><li><strong>Chi phí duy trì rẻ hơn rất nhiều</strong><br>- Khi mới kinh doanh việc đầu tự quá nhiều chi phí để thuê 1 bên thứ 3 làm website bán hàng cho bạn chi phí phải bỏ ra khá cao như như thiết kế giao diện, hosting hằng năng, phí bảo chì, rồi cài đặt plugin phải lên đến 5tr → 10tr là gói cơ bản (chưa tính phí duy trì), muốn làm thêm tính năng thì phải thêm tiền<br>- Vậy nên việc bạn có thể tự tạo làm 1 website có thể tiết kiệm rất nhiều tiền, chỉ phải bỏ ra phí ban đầu là tên miền, Thuê VPS theo tháng phí ban đầu chỉ quanh tầm 500k</li><li><strong>Chủ động trong việc cài plugin, thêm tính năng, không cần mất phí</strong><br>- Nếu bạn sử dụng wordpress để tạo website có thì sẽ có rất nhiều plugin bạn có thể thao tác cài 1 click nhanh chóng, không phải phụ thuộc bên thứ 3 sẽ tiết kiếm rất nhiều tiền sau này, plugin phong phú hơn rất nhiều so với các bên làm web</li><li><strong>Tạo được nhiều website cùng 1 lúc</strong><br>- Khi bạn chủ động mua cài website, Tài nguyên VPS do mình cầm nên mình hoàn toàn có thể build thêm rất nhiều website tương tự khác, trong trường hợp cần thay đổi tên miền, hay website cũng không bị bên dịch vụ moi tiền</li><li><strong>Chủ động tăng tải server khi có nhiều người dùng dễ dàng</strong><br>- Đa phân khi mới kinh doanh, chủ doanh nghiệp hoặc cá nhân chỉ cần 1 website chịu tải thấp tiết kiệm chi phí, nhưng lại vô cùng lãng phí khi sử dụng dịch vụ bên thứ 3 để làm website tốn đến hàng chục triệu đồng, Nhờ tự kiểm soát được VPS (hosting) mà người dùng tiết kiệm thêm rất nhiều tiền cho phần này, đôi khi chưa cần thiết có thể cho dừng website để tiết kiệm hoàn toàn chi phí</li></ol><h2>💡 Hoàn toàn có thể tạo website mà không cần biết lập trình?</h2><ol><li>Bạn chỉ cần đăng ký tên miền, và VPS theo hướng dẫn ở trong tài liệu này, việc tạo website còn lại ra do công cụ làm, theo tác thêm vài bước để tạo SSL là có ngay 1 website hoàn chỉnh cho bạn thao tác</li></ol><h2>&nbsp;</h2>`
+  }
+  ,{
+  id: '2',
+  sidebar: 'Chọn tên miền phù hợp',
+  title: 'Chọn tên miền phù hợp',
   video: 'https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4',
-  description: `<video id='video-js1' key='video-js1' class="video-js" data-setup='{"fluid": true}' controls>
+  description: `<video id='video-js2' key='video-js2' class="video-js" data-setup='{"fluid": true}' controls>
   <source src="https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4" type="video/mp4">
   </video>
-  <br>
-  <h1 id="e2eeea5c31e438d10f52993297f5edc06">Discover the riches of our editor ✨</h1>
-  Read on to better understand the functionalities you can test with this demo.
-  <h2 id="e9ae557e2bd3e09bad12bcdb00cd6ee0c">💡 Did you know that…</h2>
-  <ul>
-     <li>CKEditor is <strong>customizable</strong>. You can fine-tune the set of enabled plugins, available toolbar buttons, and the behavior of features.</li>
-     <li>The editor supports <strong>@mentions</strong>. Start typing <code>@An</code> to see available suggestions.</li>
-     <li>You can <strong>paste content</strong> from Word or Google Docs, retaining the original document structure and formatting.</li>
-     <li>Thanks to Import from Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/import_word.png 2x" alt="Import from Word" width="20" height="20" />, you can <strong>convert</strong> a DOCX document into HTML and edit it in CKEditor 5.</li>
-     <li>You can <strong>export your document</strong> to PDF <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_pdf.png 2x" alt="Export to PDF" width="20" height="20" /> or Word <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/export_word.png 2x" alt="Export to Word" width="20" height="20" /> with a single click.</li>
-     <li>This demo showcases <a href="https://ckeditor.com/ckbox/" target="_blank" rel="noopener noreferrer">CKBox</a> <img src="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png" sizes="100vw" srcset="https://ckeditor.com/assets/images/ckdemo/feature-rich/browse-files.png 2x" alt="Browse files" width="20" height="20" /> to <strong>manage images and other files</strong>. You can enable your own upload adapter instead.</li>
-  </ul>
-  <h2 id="e3110f5a48c832f6e1c5696c439440fb6">🚀 Autoformatting in CKEditor 5</h2>
-  Some features of CKEditor 5 might be hard to spot at first glance. Thanks to <strong>autoformatting</strong>, you can use handy shortcuts in the editor to format the text on the fly:
-  <figure class="table">
-  <table class="ck-table-resized"><colgroup><col /><col /><col /></colgroup>
-  <tbody>
-  <tr>
-  <td rowspan="7"><strong>Block formatting</strong></td>
-  <td>Bulleted list</td>
-  <td>Start a line with <code>*</code> or <code>-</code> followed by a space.</td>
-  </tr>
-  <tr>
-  <td>Numbered list</td>
-  <td>Start a line with <code>1.</code> or <code>1)</code> followed by a space.</td>
-  </tr>
-  <tr>
-  <td>To-do list</td>
-  <td>Start a line with <code>[ ]</code> or <code>[x]</code> followed by a space to insert an unchecked or checked list item.</td>
-  </tr>
-  <tr>
-  <td>Headings</td>
-  <td>Start a line with <code>#</code>, <code>##</code>, or <code>###</code> followed by a space to create a heading 1, heading 2, or heading 3.</td>
-  </tr>
-  <tr>
-  <td>Block quote</td>
-  <td>Start a line with <code>&gt;</code> followed by a space.</td>
-  </tr>
-  <tr>
-  <td>Code block</td>
-  <td>Start a line with <code></code>.</td>
-  </tr>
-  <tr>
-  <td>Horizontal line</td>
-  <td>Start a line with <code>---</code>.</td>
-  </tr>
-  <tr>
-  <td rowspan="4"><strong>Inline formatting</strong></td>
-  <td><strong>Bold</strong></td>
-  <td>Type <code>**</code> or <code>__</code> around your text.</td>
-  </tr>
-  <tr>
-  <td><i>Italic</i></td>
-  <td>Type <code>*</code> or <code>_</code> around your text.</td>
-  </tr>
-  <tr>
-  <td><code>Code</code></td>
-  <td>Type <code></code> around your text.</td>
-  </tr>
-  <tr>
-  <td><s>Strikethrough</s></td>
-  <td>Type <code>~~</code> around your text.</td>
-  </tr>
-  </tbody>
-  </table>
-  </figure>
-  &nbsp;`
+  <hr>
+  <h2 id="e33981ec17b5480108f981996fda52978">✨T&ecirc;n miền l&agrave; g&igrave;?</h2>
+<p style="margin-left: 40px;">T&ecirc;n miền l&agrave; địa chỉ điện tử tr&ecirc;n Internet, được sử dụng để định danh v&agrave; truy cập v&agrave;o một trang web cụ thể. V&iacute; dụ, trong t&ecirc;n miền "google.com", "google" l&agrave; phần t&ecirc;n v&agrave; ".com" l&agrave; phần đu&ocirc;i.</p>
+<h2 id="eb73583350861a9c9e748e38f4aec4563">💡 C&aacute;ch chọn t&ecirc;n miền ph&ugrave; hợp</h2>
+<ol>
+<li>Li&ecirc;n quan đến t&ecirc;n v&agrave; ng&agrave;nh nghề: Chọn một t&ecirc;n miền li&ecirc;n quan trực tiếp hoặc gợi nhớ đến t&ecirc;n hoặc ng&agrave;nh nghề của bạn. Điều n&agrave;y gi&uacute;p người d&ugrave;ng dễ d&agrave;ng nhớ v&agrave; t&igrave;m thấy trang web của bạn.</li>
+<li>Ngắn v&agrave; dễ g&otilde;: Chọn một t&ecirc;n miền ngắn v&agrave; dễ g&otilde; để tr&aacute;nh nhầm lẫn v&agrave; gi&uacute;p người d&ugrave;ng dễ d&agrave;ng nhập v&agrave;o tr&igrave;nh duyệt web.</li>
+<li>Tr&aacute;nh c&aacute;c k&yacute; tự phức tạp: Hạn chế sử dụng c&aacute;c k&yacute; tự đặc biệt, số lượng dấu gạch ngang hoặc k&yacute; tự viết hoa trong t&ecirc;n miền. Điều n&agrave;y gi&uacute;p tr&aacute;nh sự nhầm lẫn v&agrave; tạo sự thuận tiện cho người d&ugrave;ng.</li>
+<li>Đu&ocirc;i t&ecirc;n miền ph&ugrave; hợp: Đu&ocirc;i t&ecirc;n miền (như .com, .net, .org) c&oacute; thể tạo ấn tượng kh&aacute;c nhau. Đu&ocirc;i .com phổ biến v&agrave; th&iacute;ch hợp cho c&aacute;c doanh nghiệp thương mại, trong khi .org thường được sử dụng cho c&aacute;c tổ chức phi lợi nhuận. Ngo&agrave;i ra, c&oacute; thể xem x&eacute;t sử dụng đu&ocirc;i t&ecirc;n miền quốc gia (như .vn cho Việt Nam) nếu kinh doanh hướng tới một thị trường cụ thể.</li>
+<li>Bảo vệ thương hiệu: Trước khi đăng k&yacute; t&ecirc;n miền, h&atilde;y kiểm tra xem c&oacute; c&aacute;c t&ecirc;n miền tương tự đ&atilde; được sử dụng hay chưa để tr&aacute;nh việc nhầm lẫn hoặc tranh chấp. Ngo&agrave;i ra, nếu thương hiệu của bạn quan trọng, h&atilde;y xem x&eacute;t đăng k&yacute; c&aacute;c biến thể t&ecirc;n miền để bảo vệ quyền sở hữu tr&iacute; tuệ của bạn.</li>
+<li>Đăng k&yacute; t&ecirc;n miền từ nh&agrave; cung cấp uy t&iacute;n: Chọn nh&agrave; cung cấp t&ecirc;n miền đ&aacute;ng tin cậy v&agrave; được c&ocirc;ng nhận để đảm bảo quyền sở hữu v&agrave; hỗ trợ kỹ thuật đầy đủ.</li>
+<li>Lưu &yacute; rằng việc chọn t&ecirc;n miền l&agrave; một qu&aacute; tr&igrave;nh quan trọng v&agrave; cần xem x&eacute;t kỹ lưỡng. Nếu bạn gặp kh&oacute; khăn hoặc cần tư vấn cụ thể, h&atilde;y tham khảo c&aacute;c chuy&ecirc;n gia hoặc nh&agrave; cung cấp dịch vụ t&ecirc;n miền để được hỗ trợ tốt nhất.</li>
+</ol>
+<h2 id="e3e52c0f380d8f9d0e58bedd11e6a360e">🚀 C&oacute; nhiều nơi mua t&ecirc;n miền uy t&iacute;n, trong video hướng dẫn tr&ecirc;n m&igrave;nh sẽ thực h&agrave;nh tại <a target="_blank" href="https://tenten.vn">tenten.vn</a><br><br></h2>
+<p>&nbsp;</p>`
 },
 {
-  id: '2',
+  id: '3',
+  sidebar: 'Đăng ký hosting',
   title: 'Đăng ký hosting',
   video: 'https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4',
   description: `
-  <video id='video-js2' key='video-js2' class="video-js" data-setup='{"fluid": true}' controls>
+  <video id='video-js3' key='video-js3' class="video-js" data-setup='{"fluid": true}' controls>
   <source src="https://zinson.vn/wp-content/uploads/2023/06/20230301_9dd5582db91ba611_399745010841_mp4_264_hd_taobao.mp4" type="video/mp4">
   </video>
   <h1 id="ea3e002196e8f6f725c5d70c523fa9da6">Discover the riches of our editor ✨</h1>
