59c08309b240a3db90557caa866a4e0d7a078b1d
duypham1-ntq <duy.pham1@ntq-solution.com.vn>
2023-07-26 15:12:32 +0700

    fix socket

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
     origin: 'https://zinson.vn',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   }
 });
 
