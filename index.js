205eddb628009a0b75859eeae5e10fe5633877df
duypham1-ntq <duy.pham1@ntq-solution.com.vn>
2023-07-26 13:40:18 +0700

    socket

--- index.js
+++ index.js
@@ -9,18 +9,29 @@ const home = require("./routes/home");
 const socketIO = require('socket.io');
 const http = require('http');
 
+
 ffmpeg.setFfmpegPath(ffmpegPath);
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const server = http.createServer(app);
 
 const io = socketIO(server, {
-  // cors: {
-  //   origin: 'https://www.tiktok.com',
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  //   credentials: true
-  // }
+  cors: {
+    origin: 'https://zinson.vn',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }
 });
 
 // Middleware để kiểm tra headers
